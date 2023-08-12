e9155b1107e9c7b9dd6fda07344ed082fdc661f7
Jisung <wltjd9972@gmail.com>
2023-08-12 23:57:22 +0900

    Committing changes in class submodule

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
-import '@/styles/globals.css'
+// import '../styles/globals.css'
+
+//모든 페이지에서 하는 공통 설정을 _app.js에서 진행
+
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
